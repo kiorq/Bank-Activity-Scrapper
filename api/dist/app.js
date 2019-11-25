d7c2201c25ff655dddf6389d1466251ac22903b6
Kirk Douglas <kirkd@uniregistry.com>
2019-11-25 08:52:19 -0500

    cleaned up files; added gitignore

--- api/dist/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const PORT = parseInt(process.env.PORT);
-new server_1.default(PORT).start();
