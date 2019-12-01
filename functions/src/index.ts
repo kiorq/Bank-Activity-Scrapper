7365bd639f7af47391f577e8838bcd86448a2843
Kirk Douglas <kirkd@uniregistry.com>
2019-12-01 12:13:03 -0500

    renamed function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import scrapperRun from "./scrapper/app";
 
 const config: any = functions.config();
 
-export const ScrapeBankInfo = functions.pubsub.schedule("every 15 minutes").onRun(async context => {
+export const ScrapeBankActivity = functions.pubsub.schedule("every 15 minutes").onRun(async context => {
     await scrapperRun(config);
     return null;
 });
